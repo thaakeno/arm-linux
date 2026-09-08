a462f3bf1f031fb4738116746240b6b08f21900d
thaakeno <thaakeno46@gmail.com>
2026-09-08 22:32:15 +0200

    vm: restart bridge for payload-ready instance

--- app/src/main/java/com/example/dreamlinux/VmSessionService.kt
+++ app/src/main/java/com/example/dreamlinux/VmSessionService.kt
@@ -46,7 +46,7 @@ class VmSessionService : Service() {
             .daemon(false)
             .processNameSuffix("vm_bridge")
             .debuggable(true)
-            .version(5)
+            .version(6)
     }
 
     private val connection = object : ServiceConnection {
