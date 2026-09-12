a16bcf9b28fb70c109c2b5b6272ff335625ffed3
thaakeno <thaakeno46@gmail.com>
2026-09-12 03:13:54 +0200

    Fix v7 scalar-vector operator declaration scope

--- app/src/main/cpp/native_vulkan_studio_v7.cpp
+++ app/src/main/cpp/native_vulkan_studio_v7.cpp
@@ -1,9 +1,9 @@
+namespace { struct Vec3; Vec3 operator*(float s,Vec3 a); }
+
 #include "native_vulkan_studio_v7_part1.inc"
 #include "native_vulkan_studio_v7_part2.inc"
-
-// V7 camera helpers use both scalar-vector multiplication orders.
-Vec3 operator*(float s,Vec3 a){return a*s;}
-
 #include "native_vulkan_studio_v7_part3.inc"
 #include "native_vulkan_studio_v7_part4.inc"
 #include "native_vulkan_studio_v7_part5.inc"
+
+namespace { Vec3 operator*(float s,Vec3 a){return a*s;} }
