e3411b6a51f0468b4ed5665a3beef078b17121c5
thaakeno <thaakeno46@gmail.com>
2026-09-12 03:11:48 +0200

    Fix v7 vector scalar helper before CI build

--- app/src/main/cpp/native_vulkan_studio_v7.cpp
+++ app/src/main/cpp/native_vulkan_studio_v7.cpp
@@ -1,5 +1,9 @@
 #include "native_vulkan_studio_v7_part1.inc"
 #include "native_vulkan_studio_v7_part2.inc"
+
+// V7 camera helpers use both scalar-vector multiplication orders.
+Vec3 operator*(float s,Vec3 a){return a*s;}
+
 #include "native_vulkan_studio_v7_part3.inc"
 #include "native_vulkan_studio_v7_part4.inc"
 #include "native_vulkan_studio_v7_part5.inc"
