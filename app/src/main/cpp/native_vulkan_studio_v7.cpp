b7928a58406c6a63e4c81dff4a798cc5380fabfa
thaakeno <thaakeno46@gmail.com>
2026-09-12 03:21:31 +0200

    Fix v7 strict native warning build

--- app/src/main/cpp/native_vulkan_studio_v7.cpp
+++ app/src/main/cpp/native_vulkan_studio_v7.cpp
@@ -1,9 +1,12 @@
 namespace { struct Vec3; Vec3 operator*(float s,Vec3 a); }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 #include "native_vulkan_studio_v7_part1.inc"
 #include "native_vulkan_studio_v7_part2.inc"
 #include "native_vulkan_studio_v7_part3.inc"
 #include "native_vulkan_studio_v7_part4.inc"
 #include "native_vulkan_studio_v7_part5.inc"
+#pragma clang diagnostic pop
 
 namespace { Vec3 operator*(float s,Vec3 a){return a*s;} }
