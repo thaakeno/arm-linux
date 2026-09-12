451ea9871930e3f35fc1ce4b698d192a1d673da0
thaakeno <thaakeno46@gmail.com>
2026-09-12 23:04:46 +0200

    Restore Vulkan present info compatibility in v10 renderer

--- app/src/main/cpp/native_vulkan_studio_v7.cpp
+++ app/src/main/cpp/native_vulkan_studio_v7.cpp
@@ -5,7 +5,9 @@ namespace { struct Vec3; Vec3 operator*(float s,Vec3 a); }
 #include "native_vulkan_studio_v7_part1.inc"
 #include "native_vulkan_studio_v7_part2.inc"
 #include "native_vulkan_studio_v7_part3.inc"
+#define VK_STRUCTURE_TYPE_PRESENT_INFO VK_STRUCTURE_TYPE_PRESENT_INFO_KHR
 #include "native_vulkan_studio_v7_part4.inc"
+#undef VK_STRUCTURE_TYPE_PRESENT_INFO
 #include "native_vulkan_studio_v7_part5.inc"
 #pragma clang diagnostic pop
 
